01b8b3af43dbc6a2249f65620f8401a4799715fc
dummy <2010817210017@mhs.ulm.ac.id>
2023-06-16 17:09:23 +0800

    .

---
+++ app/src/main/java/com/example/nutriscan/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nutriscan.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nutriscan.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
